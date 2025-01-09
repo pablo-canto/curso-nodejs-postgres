d5ef45a013fdf6b0d088ec27ba210c096894dac6
pablo-canto <60751546+pablo-canto@users.noreply.github.com>
2025-01-09 15:47:22 +0200

    06-step
    ¿Qué es un ORM? Instalación y configuración de Sequelize ORM

--- services/product.service.js
+++ services/product.service.js
@@ -1,15 +1,12 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
-const pool = require ('../libs/postgres.pool')
+const sequelize = require ('../libs/sequelize');
 
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error',(err) => console.error(err));
-
   }
 
   generate() {
@@ -36,8 +33,15 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    //const rta = await this.pool.query(query);
+    //const [data, metadata] = await sequelize.query(query);
+    const [data] = await sequelize.query(query);
+    //return rta.rows;
+   /*  return {
+      data,
+      metadata
+    }; */
+    return data;
     // return this.products;
   }
 
