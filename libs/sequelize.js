d81d6b3148973d8023fce25703bb13e4afbbf6c0
pablo-canto <60751546+pablo-canto@users.noreply.github.com>
2025-01-09 16:41:49 +0200

    07-step
    Tu primer modelo en Sequelize

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
+const setupModels = require('./../db/models')
 
 
 const USER = encodeURIComponent(config.dbUser);
@@ -8,7 +9,10 @@ const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${
 
 const sequelize  = new Sequelize(URI,{
   dialect: 'postgres',
-  logging: false,
+  logging: true,
 });
+
+setupModels(sequelize);
+sequelize.sync();
 
 module.exports = sequelize;
